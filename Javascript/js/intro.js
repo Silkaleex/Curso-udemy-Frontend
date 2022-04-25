54c3d3f11ec5a7d3e1c43f6a1d7d25596e47c7ec
Silkaleex <alejandropascual1995@outlook.es>
2022-04-25 19:23:52 +0200

    udemy frontend

---
+++ Javascript/js/intro.js
@@ -0,0 +1,18 @@
+//Alerta en Javascript
+/* Comentario Multilinea */
+
+//Alerta JS
+alert("Hola Mundo!");
+alert("Hola Mundo 2!");
+
+//Consola Javascript
+/*console.log('Hola, Saludamos a la consola!');
+console.log(2+2);
+console.info('Mostrar una informacion');
+console.warn('Estamos mostrando una advertencia');
+console.error('Mostrando un error');
+console.clear('Borra toda la consola');*/
+
+//Document write
+/*Es la Raiz Principal del contenido del body, head, header, article*/
+document.write('Hola este mensaje se mostrará en mi web')
